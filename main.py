f3429a331b5f67b0c31b4972f4315d78a5312182
Alejandro <alexdhdiez@gmail.com>
2022-02-08 17:27:13 +0000

    Changes

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 from flask import Flask
 from flask import jsonify
-<<<<<<< HEAD
-=======
+
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello World! Trying continuous deployment (CD)'
-
-@app.route('/echo/<name>')
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
-
->>>>>>> 15398dc1707b2d368601ac888364cd84a865bbad
 
 def create_app():
     app = Flask(__name__)
