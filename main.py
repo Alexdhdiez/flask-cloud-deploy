a6309aef9c63d42be3033c378e5c4b4c9fc44679
Alejandro <alexdhdiez@gmail.com>
2022-02-08 22:27:52 +0000

    Fixed error in main app that wasnt letting the deployment work

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ from flask import Flask
 from flask import jsonify
 
 def create_app():
-    global app 
-    app = Flask(__name__)
+    my_app = Flask(__name__)
     
-    @app.route('/')
+    @my_app.route('/')
     def hello():
         """Return a friendly HTTP greeting."""
         print("I am inside hello world")
         return 'Hello World! Trying continuous deployment (CD)'
     
-    @app.route('/echo/<name>')
+    @my_app.route('/echo/<name>')
     def echo(name):
         print(f"This was placed in the url: new-{name}")
         val = {"new-name": name}
         return jsonify(val)
     
-    return app
+    return my_app
     
 if __name__ == '__main__':
-    my_app = create_app()
-    my_app.run(host='127.0.0.1', port=8091, debug=True)
+    app = create_app()
+    app.run(host='127.0.0.1', port=8091, debug=True)
